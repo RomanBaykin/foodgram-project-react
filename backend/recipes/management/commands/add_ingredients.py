4a1def7443c8703f4ce696abeedeec9358f50314
Roman Baykin <96991805+RomanBaykin@users.noreply.github.com>
2022-09-07 22:48:06 +0300

    Update add_ingredients.py

--- backend/recipes/management/commands/add_ingredients.py
+++ backend/recipes/management/commands/add_ingredients.py
@@ -13,9 +13,8 @@ class Command(BaseCommand):
     help = 'Adding Ingredients'
 
     def handle(self, *args, **options):
-        with open(
-                  os.path.join(
-                    BASE_DIR, 'data/ingredients.csv'), 'rb')as csv_file:
+        with open(os.path.join(
+                  BASE_DIR, 'data/ingredients.csv'), 'rb')as csv_file:
             csv_reader = csv.reader((
                 codecs.iterdecode(csv_file, 'utf-8')), delimiter=';')
             for row in csv_reader:
