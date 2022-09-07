67ab4017581699e69a8ce1b97e7b609ea68a4c5d
Roman Baykin <96991805+RomanBaykin@users.noreply.github.com>
2022-09-07 22:54:46 +0300

    Update urls.py

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,24 +7,3 @@ urlpatterns = [
     path('api/', include('users.urls')),
 ]
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="FoodGram API",
-      default_version='v1',
-      description="Документация для приложения FoodyGram",
-      # terms_of_service="URL страницы с пользовательским соглашением",
-      contact=openapi.Contact(email="baykinroman@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
-urlpatterns += [
-   re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-           schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-           name='schema-swagger-ui'),
-   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-           name='schema-redoc'),
-]
